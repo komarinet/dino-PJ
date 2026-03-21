881a56ecdc07e8d5cc861bdc3e35f3b50168f60d
komarinet <112837242+komarinet@users.noreply.github.com>
2026-03-21 10:02:00 +0900

    Update scenario.js

--- dino03/scenario.js
+++ dino03/scenario.js
@@ -1,15 +1,13 @@
 window.Scenario = {
-    currentStage: 1,
-
-    // ステージ情報
+    // ステージ基本情報
     stageInfo: {
         chapter: "第一章",
         name: "<ruby>母<rt>はは</rt></ruby>を<ruby>訪<rt>たず</rt></ruby>ねて"
     },
 
-    // バトル前会話
+    // 【戦闘前】会話データ
     preBattleTalk: [
-        { name: "ティラノ", face: "🦖", side: "left", text: "<ruby>探<rt>さが</rt></ruby>したぞッ！<ruby>お前<rt>おまえ</rt></ruby>が、お<ruby>母<rt>かあ</rt></ruby>さんを<ruby>連<rt>つ</rt></ruby>れて<ruby>行<rt>い</rt></ruby>ったのか！" },
+        { name: "ティラノ", face: "🦖", side: "left", text: "<ruby>探<rt>さが</rt></ruby>したぞッ！　お<ruby>前<rt>まえ</rt></ruby>がお<ruby>母<rt>かあ</rt></ruby>さんをさらったのか！" },
         { name: "ブラキオサウルス", face: "🦕", side: "right", text: "<ruby>毎日何頭<rt>まいにちなんとう</rt></ruby>の<ruby>恐竜<rt>きょうりゅう</rt></ruby>を<ruby>攫<rt>さら</rt></ruby>っていると<ruby>思<rt>おも</rt></ruby>う？" },
         { name: "ブラキオサウルス", face: "🦕", side: "right", text: "お<ruby>前<rt>まえ</rt></ruby>のお<ruby>母<rt>かあ</rt></ruby>さんなど<ruby>知<rt>し</rt></ruby>ったことか" },
         { name: "ティラノ", face: "🦖", side: "left", text: "<ruby>連<rt>つ</rt></ruby>れ<ruby>去<rt>さ</rt></ruby>った<ruby>恐竜<rt>きょうりゅう</rt></ruby>をどこへ<ruby>連<rt>つ</rt></ruby>れて<ruby>行<rt>い</rt></ruby>ったんだ" },
@@ -17,10 +15,10 @@ window.Scenario = {
         { name: "ティラノ", face: "🦖", side: "left", text: "だったら<ruby>力<rt>ちから</rt></ruby>ずくで<ruby>聞<rt>き</rt></ruby>き<ruby>出<rt>だ</rt></ruby>してやる！" }
     ],
 
-    // バトル後会話
+    // 【戦闘後】会話データ
     postBattleTalk: [
         { name: "ブラキオサウルス", face: "🦕", side: "right", text: "つ、<ruby>強<rt>つよ</rt></ruby>い……" },
-        { name: "ティラノ", face: "🦖", side: "left", text: "さあ<ruby>言<rt>い</rt></ruby>え！<ruby>恐竜<rt>きょうりゅう</rt></ruby>たちの<ruby>居場所<rt>いばしょ</rt></ruby>を！" },
+        { name: "ティラノ", face: "🦖", side: "left", text: "さあ<ruby>言<rt>い</rt></ruby>え！　<ruby>恐竜<rt>きょうりゅう</rt></ruby>たちの<ruby>居場所<rt>いばしょ</rt></ruby>を！" },
         { name: "ブラキオサウルス", face: "🦕", side: "right", text: "か、<ruby>火山<rt>かざん</rt></ruby>の<ruby>方<rt>ほう</rt></ruby>だ。それ<ruby>以上<rt>いじょう</rt></ruby>は<ruby>知<rt>し</rt></ruby>らん" },
         { name: "ティラノ", face: "🦖", side: "left", text: "<ruby>火山<rt>かざん</rt></ruby>か。わかった" },
         { name: "ブラキオサウルス", face: "🦕", side: "right", text: "<ruby>命<rt>いのち</rt></ruby>だけは<ruby>助<rt>たす</rt></ruby>けてくれ……" },
