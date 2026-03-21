095bd3635bb64d53a09d9494a4562d91f6abe9df
komarinet <112837242+komarinet@users.noreply.github.com>
2026-03-21 13:28:18 +0900

    Update scenario.js

--- dino03/scenario.js
+++ dino03/scenario.js
@@ -5,22 +5,22 @@ window.Scenario = {
     },
 
     preBattleTalk: [
-        { name: "ティラノ", face: "🦖", side: "left", text: "探したぞッ！　お前がお母さんを連れて行ったのか！" },
-        { name: "ブラキオサウルス", face: "🦕", side: "right", text: "毎日何頭の恐竜を攫ってると思う？" },
-        { name: "ブラキオサウルス", face: "🦕", side: "right", text: "お前の母親など知ったことか" },
-        { name: "ティラノ", face: "🦖", side: "left", text: "連れて行った恐竜はどこだ！" },
-        { name: "ブラキオサウルス", face: "🦕", side: "right", text: "お前に教えるわけないだろう" },
-        { name: "ティラノ", face: "🦖", side: "left", text: "だったら力ずくで聞き出してやる！" }
+        { name: "ティラノ", face: "🦖", side: "left", text: "<ruby>探<rt>さが</rt></ruby>したぞッ！　お<ruby>前<rt>まえ</rt></ruby>がお<ruby>母<rt>かあ</rt></ruby>さんを<ruby>連<rt>つ</rt></ruby>れて<ruby>行<rt>い</rt></ruby>ったのか！" },
+        { name: "ブラキオサウルス", face: "🦕", side: "right", text: "<ruby>毎日何頭<rt>まいにちなんとう</rt></ruby>の<ruby>恐竜<rt>きょうりゅう</rt></ruby>を<ruby>攫<rt>さら</rt></ruby>ってると<ruby>思<rt>おも</rt></ruby>う？" },
+        { name: "ブラキオサウルス", face: "🦕", side: "right", text: "お<ruby>前<rt>まえ</rt></ruby>の<ruby>母親<rt>ははおや</rt></ruby>など<ruby>知<rt>し</rt></ruby>ったことか" },
+        { name: "ティラノ", face: "🦖", side: "left", text: "<ruby>連<rt>つ</rt></ruby>れて<ruby>行<rt>い</rt></ruby>った<ruby>恐竜<rt>きょうりゅう</rt></ruby>はどこだ！" },
+        { name: "ブラキオサウルス", face: "🦕", side: "right", text: "お<ruby>前<rt>まえ</rt></ruby>に<ruby>教<rt>おし</rt></ruby>えるわけないだろう" },
+        { name: "ティラノ", face: "🦖", side: "left", text: "だったら<ruby>力<rt>ちから</rt></ruby>ずくで<ruby>聞<rt>き</rt></ruby>き<ruby>出<rt>だ</rt></ruby>してやる！" }
     ],
 
     postBattleTalk: [
-        { name: "ブラキオサウルス", face: "🦕", side: "right", text: "つ、強い" },
-        { name: "ティラノ", face: "🦖", side: "left", text: "さあ言え！　恐竜たちの居場所を！" },
-        { name: "ブラキオサウルス", face: "🦕", side: "right", text: "か、火山の方だ。それ以上は知らん" },
-        { name: "ティラノ", face: "🦖", side: "left", text: "火山か。わかった" },
-        { name: "ブラキオサウルス", face: "🦕", side: "right", text: "命だけは助けてくれ" },
-        { name: "ティラノ", face: "🦖", side: "left", text: "お前の命なんか、興味ない" },
-        { name: "ティラノ", face: "🦖", side: "left", text: "僕はお母さんを助けたいだけだ" },
-        { name: "ブラキオサウルス", face: "🦕", side: "right", text: "母親か・・・" }
+        { name: "ブラキオサウルス", face: "🦕", side: "right", text: "つ、<ruby>強<rt>つよ</rt></ruby>い" },
+        { name: "ティラノ", face: "🦖", side: "left", text: "さあ<ruby>言<rt>い</rt></ruby>え！　<ruby>恐竜<rt>きょうりゅう</rt></ruby>たちの<ruby>居場所<rt>いばしょ</rt></ruby>を！" },
+        { name: "ブラキオサウルス", face: "🦕", side: "right", text: "か、<ruby>火山<rt>かざん</rt></ruby>の<ruby>方<rt>ほう</rt></ruby>だ。それ<ruby>以上<rt>いじょう</rt></ruby>は<ruby>知<rt>し</rt></ruby>らん" },
+        { name: "ティラノ", face: "🦖", side: "left", text: "<ruby>火山<rt>かざん</rt></ruby>か。わかった" },
+        { name: "ブラキオサウルス", face: "🦕", side: "right", text: "<ruby>命<rt>いのち</rt></ruby>だけは<ruby>助<rt>たす</rt></ruby>けてくれ" },
+        { name: "ティラノ", face: "🦖", side: "left", text: "お<ruby>前<rt>まえ</rt></ruby>の<ruby>命<rt>いのち</rt></ruby>なんか、<ruby>興味<rt>きょうみ</rt></ruby>ない" },
+        { name: "ティラノ", face: "🦖", side: "left", text: "<ruby>僕<rt>ぼく</rt></ruby>はお<ruby>母<rt>かあ</rt></ruby>さんを<ruby>助<rt>たす</rt></ruby>けたいだけだ" },
+        { name: "ブラキオサウルス", face: "🦕", side: "right", text: "<ruby>母親<rt>ははおや</rt></ruby>か・・・" }
     ]
 };
