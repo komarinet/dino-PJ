48a843ec08b6d96809e4db2207e4b13e37042fb8
komarinet <112837242+komarinet@users.noreply.github.com>
2026-03-23 18:38:09 +0900

    Update store.js

--- dino03/store.js
+++ dino03/store.js
@@ -1,4 +1,4 @@
-export const VERSION = "8.15.0";
+export const VERSION = "8.16.0";
 import { createStore } from 'https://esm.sh/zustand/vanilla';
 
 export const gameStore = createStore((set) => ({
@@ -9,8 +9,10 @@ export const gameStore = createStore((set) => ({
     selectedTileKey: null,
     talkIndex: 0,
     hasBattleStarted: false,
+    
     setGameState: (state) => set({ gameState: state }),
-    setTalkIndex: (index) => set({ talkIndex: index })
+    setTalkIndex: (index) => set({ talkIndex: index }),
+    setState: (newState) => set(newState) // 一括更新用
 }));
 
 export const getStore = () => gameStore.getState();
