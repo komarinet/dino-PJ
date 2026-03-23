b074eef4049347e29a1cbeff5a825b49f88e301f
komarinet <112837242+komarinet@users.noreply.github.com>
2026-03-23 16:33:51 +0900

    Update stage01.js

--- dino03/data/stage01.js
+++ dino03/data/stage01.js
@@ -1,10 +1,8 @@
-window.StageData = {
+export const StageData = {
     info: {
         chapter: "第一章",
         name: "<ruby>母<rt>はは</rt></ruby>を<ruby>訪<rt>たず</rt></ruby>ねて"
     },
-
-    // ★ユニットにレベル(level)を追加、敵を追加配置
     units: [
         { id: "ティラノ", emoji: "🦖", x: 12, z: 15, hp: 30, mp: 10, str: 10, def: 8, spd: 5, mag: 5, move: 4, jump: 3, isPlayer: true, level: 1 },
         { id: "コンプソグナトゥスA", emoji: "🦎", x: 10, z: 10, hp: 15, mp: 0, str: 8, def: 5, spd: 6, mag: 0, move: 5, jump: 4, isPlayer: false, level: 1 },
@@ -13,8 +11,6 @@ window.StageData = {
         { id: "コンプソグナトゥスD", emoji: "🦎", x: 15, z: 6, hp: 15, mp: 0, str: 8, def: 5, spd: 6, mag: 0, move: 5, jump: 4, isPlayer: false, level: 1 },
         { id: "ブラキオサウルス", emoji: "🦕", x: 12, z: 3, hp: 60, mp: 10, str: 20, def: 15, spd: 4, mag: 2, move: 4, jump: 2, isPlayer: false, level: 5 }
     ],
-
-    // ★戦闘前シナリオ：新バージョン★
     preBattleTalk: [
         { name: "ティラノ", face: "🦖", text: "探したぞッ！　お前たちが『ブラキーズ』か！" },
         { name: "コンプソグナトゥスA", face: "🦎", text: "なんだァ、お前" },
@@ -26,7 +22,6 @@ window.StageData = {
         { name: "コンプソグナトゥスC", face: "🦎", text: "痛い目見る前にさっさと帰りな、坊や" },
         { name: "ティラノ", face: "🦖", text: "あくまでも言わないつもりかっ！<br>だったら力ずくで聞き出してやる！" }
     ],
-
     postBattleTalk: [
         { name: "ブラキオサウルス", face: "🦕", text: "つ、強い" },
         { name: "ティラノ", face: "🦖", text: "さあ言え！　恐竜たちの居場所を！" },
@@ -37,12 +32,12 @@ window.StageData = {
         { name: "ティラノ", face: "🦖", text: "僕はお母さんを助けたいだけだ" },
         { name: "ブラキオサウルス", face: "🦕", text: "母親か・・・" }
     ],
-
     generateLayout: function() {
+        const MAP_W = 25, MAP_D = 25;
         let data = [];
-        for (let z = 0; z < window.MAP_D; z++) {
+        for (let z = 0; z < MAP_D; z++) {
             data[z] = [];
-            for (let x = 0; x < window.MAP_W; x++) {
+            for (let x = 0; x < MAP_W; x++) {
                 let h = 2; let type = 0;
                 if (z >= 16) { h = 0; type = 4; }
                 if (z < 16) { h = 2; type = 0; }
